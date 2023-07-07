a734c6431aa8bf6520742d37d65b9c7e52d2843a
Ritika Kapoor <ritikakapoor.imp@gmail.com>
2023-07-08 00:55:02 +0530

    Configure spring data rest properties
    Added base-path, default-page-size and rest resource path(commented) for spring data rest end-points

--- src/main/java/com/example/springboot/cruddemo/springjpa/EmployeeRepository.java
+++ src/main/java/com/example/springboot/cruddemo/springjpa/EmployeeRepository.java
@@ -2,7 +2,12 @@ package com.example.springboot.cruddemo.springjpa;
 
 import com.example.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// this will change the rest resource path
+// this will now expose http://localhost:8080/data-rest/members
+// 'members' instead of 'employees'
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // no need to write any code as we get the
     // crud methods for free
